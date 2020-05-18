1a646f3212540b493f473fca57ac0a814b3ce423
artbred <artbred01@gmail.com>
2020-05-18 21:11:42 +0300

    refactored groups views

--- groups/views.py
+++ groups/views.py
@@ -7,47 +7,15 @@ from groups.models import Group
 from django.views.generic import View
 
 
-# @login_required(login_url='/account/login')
-# def dashboard_groups(request):
-#
-#     context_form = dict()
-#     context = dict()
-#
-#     if request.method == 'POST':
-#         context_form['form'] = add_group(request)
-#
-#     user_groups = Group.objects.filter(manager=request.user).all()
-#
-#     if len(user_groups) == 0:
-#         context['no_groups'] = True
-#     else:
-#         context['groups'] = user_groups
-#
-#     context.update(context_form)
-#     return render(request, 'dashboard/groups/groups.html', context)
-
-# @login_required(login_url='/account/login')
-# def add_group(request):
-#
-#     group_creation_form = GroupCreationForm()
-#     user = request.user
-#
-#     if request.method == "POST":
-#         group_creation_form = GroupCreationForm(request.POST)
-#         if group_creation_form.is_valid():
-#             group = group_creation_form.save(commit=False)
-#             group.manager = user
-#             group.save()
-#
-#     return group_creation_form
-
-
 class DashboardGroupsView(View):
+
+    @login_required(login_url='/account/login')
     def get(self, request):
         user_groups = Group.objects.filter(manager=request.user).all()
         group_creation_form = GroupCreationForm()
         return render(request, 'dashboard/groups/groups.html',context={'form': group_creation_form, 'groups': user_groups})
 
+    @login_required(login_url='/account/login')
     def post(self, request):
         user_groups = Group.objects.filter(manager=request.user).all()
         user = request.user
@@ -58,7 +26,9 @@ class DashboardGroupsView(View):
             group.manager = user
             group.save()
             user_groups = Group.objects.filter(manager=request.user).all()
-            return render(request, 'dashboard/groups/groups.html', context={'form': group_creation_form, 'groups': user_groups})
+            return render(request, 'dashboard/groups/groups.html', context={'form': group_creation_form, 'groups':
+                user_groups})
+
         return render(request, 'dashboard/groups/groups.html',
                       context={'form': group_creation_form, 'groups': user_groups})
 
--- message/models.py
+++ message/models.py
@@ -1,1 +1,8 @@
 from django.db import models
+
+
+class TextMessageModel(models.Model):
+
+    group = models.ForeignKey('groups.Group', related_name='group', on_delete=models.CASCADE)
+    price = models.FloatField(default=5.0, editable=False, blank=True)
+    text = models.CharField(max_length=255)
